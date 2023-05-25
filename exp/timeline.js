a8bb562ef5d8ae80745d13aaea2c71b252e0260d
joshkenney <joshuagkenney@gmail.com>
2023-05-25 19:58:57 -0400

    wrapper integration complete

---
+++ exp/timeline.js
@@ -0,0 +1,78 @@
+timeline = [];
+
+let introduction = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus:
+        "You will see a series of images. Press the space bar when you think you see something. Press the space bar to begin.",
+    choices: "ALL_KEYS",
+};
+timeline.push(introduction);
+
+let init_response = 1;
+
+let preload = {
+    type: jsPsychPreload,
+    auto_preload: true,
+};
+
+let trials = {
+    type: jsPsychImageKeyboardResponse,
+    choices: " ",
+    // prompt: "<p>Press the space bar if you see an object",
+    trial_duration: 500,
+    stimulus: jsPsych.timelineVariable("stimulus"),
+    post_trial_gap: 0,
+    // render_on_canvas: true,
+    on_finish: function (data) {
+        if (jsPsych.pluginAPI.compareKeys(data.response, " ")) {
+            init_response = 1;
+        } else {
+            init_response = 0;
+        }
+    },
+};
+
+let response_trial = {
+    type: jsPsychSurveyText,
+    questions: [{ prompt: "What did you see?" }],
+};
+
+let certainty_trial = {
+    type: jsPsychHtmlSliderResponse,
+    stimulus: "<p>How certain are you?",
+    require_movement: true,
+    labels: ["0%", "50%", "100%"],
+};
+
+let response_initiated = {
+    timeline: [response_trial, certainty_trial],
+    conditional_function: function () {
+        if (init_response == 1) {
+            return true;
+        }
+        return false;
+    },
+};
+
+let test_procedure = {
+    timeline: [trials, response_initiated],
+    timeline_variables: test_stimuli,
+};
+timeline.push(test_procedure);
+
+let save_data = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus:
+        "<p>Data saving...</p>" +
+        "<p>Do not close this window until the text dissapears.</p>",
+
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 5000,
+    on_finish: function () {
+        "use strict";
+        saveData("apophenia_" + workerId, jsPsych.data.get().csv());
+    },
+};
+timeline.push(save_data);
+
+jsPsych.run(timeline);
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>Apophenia task v2</title>
 
     <script src="db/validate.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <script src="https://unpkg.com/jspsych@7.0.0"></script>
     <script src="https://unpkg.com/@jspsych/plugin-html-keyboard-response@1.0.0"></script>
     <script src="https://unpkg.com/@jspsych/plugin-image-keyboard-response@1.0.0"></script>
@@ -19,8 +20,19 @@
     <link href="https://unpkg.com/jspsych@7.0.0/css/jspsych.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css" />
 </head>
-<script src="fn.js"></script>
-<script src="conf.js"></script>
+<script src="exp/fn.js"></script>
+<script src="exp/conf.js"></script>
+<?php
+      if ($db_connection_status == true) {
+        include_once "include/nda.php";
+        // echo'<br>';
+        // echo'connected';
+      } else if ($db_connection_status == false) {
+        include_once "include/intake.php";
+        // echo'<br>';
+        // echo'not connected';
+      }
+    ?>
 <script>
 
     const jsPsych = initJsPsych({
@@ -28,84 +40,6 @@
         //on_finish: function(){saveData(jsPsych.data.get().csv());}
     });
 
-    timeline = [];
-
-    let introduction = {
-        type: jsPsychHtmlKeyboardResponse,
-        stimulus: "You will see a series of images. Press the space bar when you think you see something. Press the space bar to begin.",
-        choices: "ALL_KEYS"
-    }
-    timeline.push(introduction)
-
-
-
-    let init_response = 1;
-
-    let preload = {
-        type: jsPsychPreload,
-        auto_preload: true
-    }
-
-    let trials = {
-        type: jsPsychImageKeyboardResponse,
-        choices: " ",
-        // prompt: "<p>Press the space bar if you see an object",
-        trial_duration: 500,
-        stimulus: jsPsych.timelineVariable('stimulus'),
-        post_trial_gap: 0,
-        // render_on_canvas: true,
-        on_finish: function (data) {
-            if (jsPsych.pluginAPI.compareKeys(data.response, ' ')) {
-                init_response = 1;
-            } else {
-                init_response = 0;
-            }
-        }
-    };
-
-    let response_trial = {
-        type: jsPsychSurveyText,
-        questions: [
-            { prompt: 'What did you see?' }
-        ]
-    }
-
-    let certainty_trial = {
-        type: jsPsychHtmlSliderResponse,
-        stimulus: "<p>How certain are you?",
-        require_movement: true,
-        labels: ['0%', '50%', '100%']
-    }
-
-    let response_initiated = {
-        timeline: [response_trial, certainty_trial],
-        conditional_function: function () {
-            if (init_response == 1) { return true; }
-            return false;
-        }
-    }
-
-    let test_procedure = {
-        timeline: [trials, response_initiated],
-        timeline_variables: test_stimuli,
-    };
-    timeline.push(test_procedure)
-
-    let save_data = {
-        type: jsPsychHtmlKeyboardResponse,
-        stimulus: "<p>Data saving...</p>" +
-            "<p>Do not close this window until the text dissapears.</p>",
-
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 5000,
-        on_finish: function () {
-            'use strict';
-            saveData("apophenia_" + workerId, jsPsych.data.get().csv());
-        }
-    };
-    timeline.push(save_data)
-
-    jsPsych.run(timeline);
 
 
 </script>
